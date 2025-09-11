73368e66db9783720bb24e9533983a9bc174cd29
MadBlackHatter <132512997+tom7166@users.noreply.github.com>
2025-09-11 16:41:15 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
+import App from"./App";
+import ReactDOM from "react-dom/client";
+import React from "react";
 
+ReactDOM.createRoot (document.getELementById("root")!).render(
+  <React.StrictMode>
 <BrowserRouter basename="/aaron-michael-services">
   <App />
 </BrowserRouter>
-
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+  </React.StrictMode>
+  );
