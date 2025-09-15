5a04ec0da6d4053929ba9b38fcb2769f88b11d8a
MadBlackHatter <132512997+tom7166@users.noreply.github.com>
2025-09-15 15:06:35 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  base: '/aging-in-place/',
+  build: {
+    outDir: 'dist',
   },
 });
