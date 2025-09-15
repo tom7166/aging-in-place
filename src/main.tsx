c8b6b42579ce377724429bca742873392848872c
MadBlackHatter <132512997+tom7166@users.noreply.github.com>
2025-09-15 15:38:19 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { HashRouter } from "react-router-dom";
-<BrowserRouter basename="/aging-in-place">
-<App />
-</BrowserRouter>
-import App from"./App";
-import ReactDOM from "react-dom/client";
-import React from "react";
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM.createRoot (document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-<BrowserRouter basename="/aging-in-place">
-  <App />
-</BrowserRouter>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
-  );
+);
+
