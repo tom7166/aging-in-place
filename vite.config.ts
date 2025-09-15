a1aa3a4f301a1d6017260ed14d2f7d93824045db
MadBlackHatter <132512997+tom7166@users.noreply.github.com>
2025-09-15 15:20:12 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
+import react from '@vite/plugin-react';
 
 export default defineConfig({
+  plugins:[react()]
   base: '/aging-in-place/',
   build: {
     outDir: 'dist',
