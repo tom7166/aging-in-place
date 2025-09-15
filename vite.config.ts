6b72a8de946fdc32a4a5acd01c7f8ead821ce22c
MadBlackHatter <132512997+tom7166@users.noreply.github.com>
2025-09-15 15:21:56 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
-import react from '@vite/plugin-react';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins:[react()]
+  plugins: [react()],
+  
+  // Set the base path to your GitHub Pages repo
   base: '/aging-in-place/',
+
+  server: {
+    port: 5173, // optional, default port for dev server
+  },
+
   build: {
     outDir: 'dist',
+    sourcemap: true,
   },
 });
