0e54730e9fb6495a9d5bedd484e920f00823dbb4
MadBlackHatter <132512997+tom7166@users.noreply.github.com>
2025-09-15 15:24:10 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 <BrowserRouter basename="/aging-in-place">
 <App />
 </BrowserRouter>
