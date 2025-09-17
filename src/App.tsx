21b2e8db15fbd0b1c8966f1fa0dcdc6e0786f005
MadBlackHatter <132512997+tom7166@users.noreply.github.com>
2025-09-16 22:54:43 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 import { localBusinessSchema, generateFAQSchema, injectSchema } from './utils/schema';
 import { FAQS } from './utils/constants';
 
